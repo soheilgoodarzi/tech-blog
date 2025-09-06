10747a4d42feac1137ffcc3f26f9d2432cc778e2
soheilgoodarzi <soheilgoodarziii@gmail.com>
2025-09-06 20:37:56 +0330

    fix props type in PostPage

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,8 +3,10 @@ import { notFound } from "next/navigation"
 import Image from "next/image"
 import Link from "next/link"
 import { Metadata } from "next"
+
+// ✅ generateStaticParams باید async باشد و await روی getAllPosts اعمال شود
 export async function generateStaticParams() {
-  const posts = getAllPosts()
+  const posts = await getAllPosts()
   return posts.map((post) => ({
     slug: post.slug,
   }))
@@ -13,13 +15,16 @@ export async function generateStaticParams() {
 type MetadataProps = {
   params: { slug: string }
 }
+
 export async function generateMetadata({
   params,
 }: MetadataProps): Promise<Metadata> {
   const post = await getPostBySlug(params.slug)
+
   if (!post) {
     return { title: "Post Not Found" }
   }
+
   return {
     title: `${post.title} | TechBlog by Soheil`,
     description: post.excerpt,
@@ -27,21 +32,18 @@ export async function generateMetadata({
 }
 
 type PageProps = {
-  params: {
-    slug: string
-  }
+  params: { slug: string }
 }
+
 export default async function PostPage({ params }: PageProps) {
   const post = await getPostBySlug(params.slug)
 
-  if (!post) {
-    notFound()
-  }
+  if (!post) notFound()
 
   return (
     <div className="max-w-[95%] md:max-w-[80%] mx-auto py-8 px-4">
       <article>
-        <header className="relative w-full h-52 md:h-72 lg:h-96 rounded-2xl overflow-hidden ">
+        <header className="relative w-full h-52 md:h-72 lg:h-96 rounded-2xl overflow-hidden">
           <Image
             src={post.coverImage}
             alt={`Cover image for ${post.title}`}
@@ -59,11 +61,13 @@ export default async function PostPage({ params }: PageProps) {
             </p>
           </div>
         </header>
+
         <div
           className="md:mt-8 mt-3 text-xs md:text-base prose prose-invert lg:prose-xl max-w-none md:ml-4 ml-1 mx-auto prose-h2:text-3xl prose-p:leading-relaxed prose-a:text-blue-400 hover:prose-a:text-blue-300"
           dangerouslySetInnerHTML={{ __html: post.contentHtml }}
         />
-        <div className="mt-4 pt-2 border-t border-gray-700  mx-auto">
+
+        <div className="mt-4 pt-2 border-t border-gray-700 mx-auto">
           <h3 className="text-base md:text-lg font-semibold text-gray-300 mb-4">
             Tags
           </h3>
@@ -74,7 +78,7 @@ export default async function PostPage({ params }: PageProps) {
                 href={`/?tag=${tag}`}
                 className="bg-gray-700 text-gray-200 hover:bg-blue-500 hover:text-white transition-colors text-xs md:text-sm font-medium px-3 py-1.5 rounded-full"
               >
-                # {tag}
+                #{tag}
               </Link>
             ))}
           </div>
